619140c7afb5da14668b27b4aa4e490c8ad8b1d6
Hari Shankar Singh <hari@lmg8.tech>
2025-08-26 08:38:57 +0530

    Add project files.

---
+++ Valid Parentheses/Program.cs
@@ -0,0 +1,22 @@
+﻿class program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(IsValidParentheses("()")); // output true
+        Console.WriteLine(IsValidParentheses("()[]{}")); //output true
+        Console.WriteLine(IsValidParentheses("(]")); //output false
+        Console.WriteLine(IsValidParentheses("([])"));  //output true
+    }
+    public static bool IsValidParentheses(string p)
+    {
+        string s = p;
+        while (s.Contains("()") || s.Contains("{}")|| s.Contains("[]"))
+        {
+            s=s.Replace("()", "")
+                 .Replace("{}", "")
+                 .Replace("[]", "");
+
+        }
+        return s.Length==0;
+    }
+}
